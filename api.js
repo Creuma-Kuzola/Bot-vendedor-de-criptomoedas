773f4af4fead314c1fef362c01005fb5e770c319
Creuma Kuzola <creumakuzola@gmail.com>
2021-06-02 14:20:00 +0100

    Classe MercadoBitcoin done e fazendo a classe MercadoBitcoinTrade

--- api.js
+++ api.js
@@ -1,5 +1,45 @@
+const ENDPOINT_API = 'https://www.mercadobitcoin.net/api/';
+const ENDPOINT_TRADE_API = 'https://www.mercadobitcoin.net'+ ENDPOINT_TRADE_PATH;
+const ENDPOINT_TRADE_PATH = '/tapi/v3/';
+
 const axios = require('axios');
-const ENDPOINT_API = 'https://www.mercadobitcoin.net/api/'
+const qs = require('querystring');
+const crypto = require('crypto');
+
+class MercadoBitcoinTrade{
+
+    constructor(config){
+        this.config = {
+            KEY: config.key,
+            SECRET: config.secret,
+            PIN: config.pin,
+            CURRENCY: config.currency
+        }
+    }
+
+    async call(method, parameters){
+        const now = new Date().getTime();
+        let queryString = qs.stringify({tapi_method:method, tapi_nonce:now});
+        
+        if(parameters) {
+            queryString += `&${qs.stringify(parameters)}`;
+        }
+
+        const signature = crypto.createHmac('sha512', this.config.SECRET)
+            .update(`${ENDPOINT_TRADE_PATH}?${queryString}`)
+            .disgest('hex');
+
+        const config = {
+
+            headers: {
+                'TAPI-ID': this.config.KEY,
+                'TAPI-MAC':signature
+            }
+        }
+        
+        const response = await axios.post(ENDPOINT_TRADE_API, queryString, config)
+    }
+}
 
 class MercadoBitcoin{
 
@@ -34,4 +74,6 @@ class MercadoBitcoin{
     }
 }
 
- module.exports = MercadoBitcoin;
+ module.exports ={
+    MercadoBitcoin 
+ } 
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require('dotenv-safe').config();
 
-setInterval(() =>{
+const { MercadoBitcoin } = require('./api');
+const infoApi = new MercadoBitcoin({currency: 'BTC'});
 
-    console.log('ping');
+setInterval( async() =>{
+    const response = await infoApi.ticker();
+    console.log(response);
+
+    if(response.ticker.sell > 33900){
+        return console.log('Ta caro, aguardar');  
+    }
+    else{
+        return console.log('Ta barato, comprar');  
+    }
 
 }, process.env.CRAWLER_INTERVAL)
