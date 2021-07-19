b0c9369fd428e1bb3b83b959a2e86a19799eac25
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-07-19 01:59:04 +0100

    Bot quase feito

--- api.js
+++ api.js
@@ -1,85 +1,119 @@
-const ENDPOINT_API = 'https://www.mercadobitcoin.net/api/';
-const ENDPOINT_TRADE_API = 'https://www.mercadobitcoin.net'+ ENDPOINT_TRADE_PATH;
-const ENDPOINT_TRADE_PATH = '/tapi/v3/';
+const TRADE_API = 'http://localhost:3001/';
+
 
 const axios = require('axios');
 const qs = require('querystring');
 const crypto = require('crypto');
+const BigNumber = require('big.js');
+const defaultDecimalPlace = '000000000000000000';
 
-class MercadoBitcoinTrade{
-
-    constructor(config){
+class MercadoBitcoinTrade {
+    constructor(config) {
         this.config = {
-            KEY: config.key,
-            SECRET: config.secret,
-            PIN: config.pin,
-            CURRENCY: config.currency
+            PRIVATE_KEY: config.privateKey,
+            ACCOUNT_NUMBER: config.accountSecret,
+            STUDENT_ID: config.studentId,
+            NAME: config.name
         }
     }
 
-    getAccountInfo(){
-        return this.call('get_account_info',{});
-    }
-
-    placeBuyOrder(qty, limit_price){
-
-        return this.call('place_buy_order',{
-            coin_pair: `BRL${this.config.CURRENCY}`,
-            quantity: `${qty} `.substr(0,10),
-            limit_price: `${limit_price}`
+    balance() {
+        return this.call('GET', 'balance/' + this.config.STUDENT_ID, {
+            "idConta": this.config.ACCOUNT_NUMBER,
+            "privateKey": this.config.PRIVATE_KEY
         })
     }
 
-    placeSellOrder(qty, limit_price){
-        return this.call('place_sell_order',{
-            coin_pair: `BRL${this.config.CURRENCY}`, 
-            quantity: `${qty}` .substr(0,10),
-            limit_price: `${limit_price}`
+    exchangePrices() {
+        return this.call('GET', 'exchange', {
+            "idConta": this.config.ACCOUNT_NUMBER
         })
-
     }
 
-    async call(method, parameters){
-        const now = new Date().getTime();
-        let queryString = qs.stringify({tapi_method:method, tapi_nonce:now});
-        
-        if(parameters) {
-            queryString += `&${qs.stringify(parameters)}`;
-        }
+    getExchangeBuyOrders() {
+        return this.call('GET', 'getBuyOrders', {})
+    }
 
-        const signature = crypto.createHmac('sha512', this.config.SECRET)
-            .update(`${ENDPOINT_TRADE_PATH}?${queryString}`)
-            .disgest('hex');
+    getExchangeSellOrders() {
+        return this.call('GET', 'getSellOrders', {})
+    }
 
-        const config = {
+    getMyBuyOrders() {
+        return this.call('GET', 'getBuyOrders/' + this.config.ACCOUNT_NUMBER, {
+            "privateKey": this.config.PRIVATE_KEY
+        })
+    }
 
-            headers: {
-                'TAPI-ID': this.config.KEY,
-                'TAPI-MAC':signature
-            }
-        }
-        
-        const response = await axios.post(ENDPOINT_TRADE_API, queryString, config)
-        if(response.data.error_message) throw new Error(response.data.error_message)
-        return response.data.response_data;
+    getMySellOrders() {
+        return this.call('GET', 'getSellOrders/' + this.config.ACCOUNT_NUMBER, {
+            "idConta": this.config.ACCOUNT_NUMBER,
+            "privateKey": this.config.PRIVATE_KEY
+        })
+    }
+
+    placeBuyOrder(targetTokenName, offeredTokenName, quantTokensOffered, quantTokensTarget) {
+        return this.call('POST', 'placeBuyOrder', {
+            "idEstudante": this.config.STUDENT_ID,
+            "idConta": this.config.ACCOUNT_NUMBER,
+            "privateKey": this.config.PRIVATE_KEY,
+            "targetTokenName": targetTokenName,
+            "offeredTokenName": offeredTokenName,
+            "quantTokensOffered": quantTokensOffered,
+            "quantTokensTarget": quantTokensTarget,
+            "orderType": 1
+        })
+    }
+
+    placeSellOrder(targetTokenName, offeredTokenName, quantTokensOffered, quantTokensTarget) {
+        return this.call('POST', 'placeBuyOrder', {
+            "idEstudante": this.config.STUDENT_ID,
+            "idConta": this.config.ACCOUNT_NUMBER,
+            "privateKey": this.config.PRIVATE_KEY,
+            "targetTokenName": targetTokenName,
+            "offeredTokenName": offeredTokenName,
+            "quantTokensOffered": quantTokensOffered,
+            "quantTokensTarget": quantTokensTarget,
+            "orderType": 0
+        })
+    }
+
+    exchangeMarketMoviment() {
+        return this.call('GET', 'exchangeMarketMoviment', {})
+    }
+
+    ticker() {
+        return this.call('GET', 'exchangeMarketMoviment', {})
+    }
+
+    async call(method, endPoint, bodyData) {
+        const response = await axios({
+            url: endPoint,
+            method: method, // default
+            baseURL: TRADE_API,
+            headers: { 'X-Requested-With': 'XMLHttpRequest' },
+            data: bodyData,
+        });
+
+        console.log(response.data);
+        return response.data;
     }
 }
 
-class MercadoBitcoin{
 
-    constructor(config){
+class MercadoBitcoin {
+
+    constructor(config) {
         this.config = {
             CURRENCY: config.currency
         }
     }
 
-    ticker (){
-
+    ticker() {
         return this.call('ticker');
     }
 
-    async call(method){
-       
+    async call(method) {
+
         const config = {
             headers: {
                 'Accept': 'application/json'
@@ -87,17 +121,17 @@ class MercadoBitcoin{
         }
 
         try {
-            
-            const response = await axios.get(ENDPOINT_API + this.config.CURRENCY + '/' + method)
+            const response = await axios.get(TRADE_API + this.config.CURRENCY + '/' + method)
             return response.data;
         } catch (error) {
             console.error(error);
             return false;
         }
-        
+
     }
 }
 
- module.exports ={
-    MercadoBitcoin 
- } 
+module.exports = {
+    MercadoBitcoin,
+    MercadoBitcoinTrade
+}
--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 require('dotenv-safe').config();
 
 const { MercadoBitcoin, MercadoBitcoinTrade } = require('./api');
-const infoApi = new MercadoBitcoin({currency: 'BTC'});
-const tradeApi = new MercadoBitcoinTrade({
+const infoApi = new MercadoBitcoin({ currency: 'BTC' });
 
-    currency: 'BTC',
-    key: process.env.KEY, 
-    secret: process.env.SECRET,
-    pin: process.env.PIN
+const tradeApi = new MercadoBitcoinTrade({
+    privateKey: process.env.PRIVATE_KEY,
+    accountSecret: process.env.ACCOUNT_NUMBER,
+    studentId: process.env.STUDENT_ID,
+    name: process.env.NAME
 })
 
-async function getQuantity(coin, price, isBuy){
-
+async function getQuantity(coin, price, isBuy) {
     price = parseFloat(price);
     coin = isBuy ? 'brl' : coin.toLowerCase();
-    const data = await tradeApi.getAccountInfo();
-    const balance = parseFloat(data.balance[coin].available).toFixed(8);
+    const data = await tradeApi.balance();
+    const balance = BigNumber(data.balances[coin]);
 
-    if(!isBuy) return balance;
 
-    if(isBuy && balance < 100) return console.error('Saldo insuficiente para comprar');
+    if (!isBuy) return balance;
+
+    if (isBuy && balance < 100) return console.error('Saldo insuficiente para comprar');
     console.log(`Saldo disponivel de ${coin}: ${balance}`);
 
     let qty = 0;
-    if(isBuy) qty = parseFloat((balance/price).toFixed(8));
+    if (isBuy) qty = parseFloat((balance / price).toFixed(8));
     return qty - 0.000000001;
 }
 
-setInterval( async() =>{
-    const response = await infoApi.ticker();
-    console.log(response);
+setInterval(async() => {
+    const responseSellOrders = await tradeApi.getExchangeSellOrders();
+    const responeBuyOrders = await tradeApi.getExchangeSellOrders();
+    /*console.log(response);
 
-    if(response.ticker.sell > 33900){
-        return console.log('Ta caro, aguardar');  
-    }
+                if (response.ticker.sell > 33900) {
+                    return console.log('Ta caro, aguardar');
+                }
 
-    try {
-      const qty = await getQuantity('BRL', response.ticker.sell,true)
-      //const data = await tradeApi.placeBuyOrder(qty, response.ticker.sell);
-      console.log(qty);
-      const buyPrice = parseFloat(response.ticker.sell);
-      const profitability = parseFloat(process.env.PROFITABILITY); //LUCRO QUE EU PRETENDO GANHAR EM CADA COMPRA
-      const data2 = await tradeApi.placeSellOrder(data.quantity, buyPrice * profitability);
-      
-    } catch (error) {
-        
-    }  
+                try {
+                    const qty = await getQuantity('BRL', response.ticker.sell, true)
+                        //const data = await tradeApi.placeBuyOrder(qty, response.ticker.sell);
+                    console.log(qty);
+                    const buyPrice = parseFloat(response.ticker.sell);
+                    const profitability = parseFloat(process.env.PROFITABILITY); //LUCRO QUE EU PRETENDO GANHAR EM CADA COMPRA
+                    const data2 = await tradeApi.placeSellOrder(data.quantity, buyPrice * profitability);
 
+                } catch (error) {
+
+                }
+*/
 }, process.env.CRAWLER_INTERVAL)
